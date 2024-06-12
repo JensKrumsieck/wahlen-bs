3c0f081d04334b6e1983d62dc18602253a3c2973
Dr. Jens Krumsieck <j.krumsieck@outlook.de>
2024-06-12 20:29:52 +0200

    fresh

--- harvester/config.py
+++
@@ -1,9 +0,0 @@
-import os;
-
-url_to2019 = "https://www3.braunschweig.de/statistik/2021_Wahl-Atlas_bis2019/data.js"
-url = "https://www3.braunschweig.de/statistik/2024_Wahl-Atlas/data.js"
-
-endpoints = [url_to2019, url]
-data_dir =  os.path.dirname(__file__) + "/data/"
-output_dir = os.path.dirname(__file__) + "/../data/"
-filenames = ["electionsTo2019.json", "electionsSince2021.json"]
